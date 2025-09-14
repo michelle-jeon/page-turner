03cf4cf024055daaf955f1c75ebdbb49b431b880
migyul <tangerine0.0tail@gmail.com>
2025-09-14 17:23:54 +0900

    debug: 헤더 생성 후 searchParams 타입 오류
    next dev 재컴파일되면서 searchParams가 동기 객체가 아닌 Promise로 전달된듯하다고 함..?

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -4,8 +4,9 @@ import { getBooksSearch } from "@/lib/books";
 // *************** 검색 결과
 export default async function Page({
   searchParams,
-}:{searchParams:{q?:string}}){
-  const q = (searchParams.q ?? "").trim();
+}:{searchParams:Promise<{q?:string}>}){
+  const params = await searchParams;
+  const q = (params.q ?? "").trim();
   if(!q) return <p>검색어 없삼;</p>
 
   const items = await getBooksSearch("Title", {max:20, start:1},q);
@@ -13,7 +14,7 @@ export default async function Page({
   return (
     <div>
       <h3>“{q}” 검색 결과</h3>
-      //씁..... 이거 컴포넌트 이거 아닌디.. 귀찮으니까 일단...
+      {/* 씁..... 이거 컴포넌트 이거 아닌디.. 귀찮으니까 일단... */}
       <BestSellerList books={items} />
     </div>
   )
