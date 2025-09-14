ca458571a533510ed3c599ed07e3700af048dfd1
migyul <tangerine0.0tail@gmail.com>
2025-09-14 14:44:14 +0900

    feat: 검색 바

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -3,42 +3,54 @@
 import { getBooksSearch } from "@/lib/books";
 import { BookItem } from "@/types";
 import { FormEvent, useState } from "react";
+import styles from "./SearchBar.module.css";
+import { useRouter } from "next/navigation";
 
 export default function SearchBar() {
   const [search,setSearch] = useState<string>("");
-  const [loading,setLoading] = useState<boolean>(false);
-  const [books,setBooks] = useState<BookItem[]>([]);
-
-  const onSubmit = async (e:FormEvent<HTMLFormElement>) =>{
+  // const [loading,setLoading] = useState<boolean>(false);
+  // const [books,setBooks] = useState<BookItem[]>([]);
+  const router = useRouter();
+  
+  const onSubmit = (e:FormEvent<HTMLFormElement>) =>{
     e.preventDefault();
-    console.log("검색어:",search);
-    setLoading(true);
+    const q = search.trim();
+    if (!q) return;
+    router.push(`/search?q=${encodeURIComponent(q)}`);
+    // setLoading(true);
 
-    if(!search.trim()){
-      alert("검색어를 입력해주세요.");
-      return;
-    }
+    // console.log("검색어:",search);
 
-    const result = await getBooksSearch(
-      "Keyword",
-      {start:1,max:10},
-      search
-    );
+    // if(!search.trim()){
+    //   alert("검색어를 입력해주세요.");
+    //   return;
+    // }
+
+    // const result = await getBooksSearch(
+    //   "Keyword",
+    //   {start:1,max:10},
+    //   search
+    // );
     
-    setBooks(result);
-    setLoading(false);
+    // setBooks(result);
+    // setLoading(false);
   }
 
   return (
-    <form className="" onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={onSubmit}>
       <input 
+        className={styles.input}
         type="text" 
         placeholder="추천: 이세계 착각 헌터"
         value={search}
         onChange={e=>setSearch(e.target.value)}
       />
-      <button type="submit" disabled={loading}>
-        {loading?"검색중...":"검색"}
+      <button 
+        type="submit" 
+        /*disabled={loading}*/
+        className={styles.button}
+      >
+        {/* {loading?"검색중...":"검색"} */} 검색
       </button>
     </form>
   )
